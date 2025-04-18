bf33366b4f6ec40479a9b0db1e135d8c0ec61888
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-18 10:40:58 +0530

    Merge Sorted Array.

---
+++ wasim2.c
@@ -0,0 +1,36 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int i=m-1;
+    int j=n-1;
+    int k=m+n-1;
+    while(i>=0 && j>=0)
+    {
+        if(nums1[i]>nums2[j])
+        {
+            nums1[k]=nums1[i];
+            k--;
+            i--;
+        }
+        else
+        {
+            nums1[k]=nums2[j];
+            k--;
+            j--;
+        }
+    }
+    while(i>=0)
+    {
+        nums1[k]=nums1[i];
+        k--;
+        i--;
+    }
+    while(j>=0)
+    {
+        nums1[k]=nums2[j];
+        k--;
+        j--;
+    }
+    for(i=0;i<m+n;i++)
+    {
+        printf("%d ",nums1[i]);
+    }
+}
