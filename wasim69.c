e06d4e590c165254266340da99949f634fe79b21
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-25 21:35:48 +0530

    Number of Steps to Reduce a Number to Zero

---
+++ wasim69.c
@@ -0,0 +1,17 @@
+int numberOfSteps(int num) {
+    int count=0;
+    while(num)
+    {
+        if(num%2==0)
+        {
+            num=num/2;
+            count++;
+        }
+        else
+        {
+            num=num-1;
+            count++;
+        }
+    }
+    return count;
+}
