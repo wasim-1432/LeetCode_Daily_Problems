bd8d833b98b019de3ca0f5adc90810ed5636c7cf
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-26 08:08:01 +0530

    Count Odd Numbers in an Interval Range.

---
+++ wasim72.c
@@ -0,0 +1,13 @@
+
+
+int countOdds(int low, int high){
+    int i,count=0;
+    for(i=low;i<=high;i++)
+    {
+        if(i%2!=0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
